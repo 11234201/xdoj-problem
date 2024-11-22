133fc5f31447c31cec3b4ccd9035e82750731744
1123 <1079733103@qq.com>
2024-11-22 21:50:26 +0800

    有很多冗余代码，在试A*算法，结果看勾八输出样例发现是要求dfs

--- main.cpp
+++ main.cpp
@@ -2,89 +2,188 @@
 
 #include <iostream>
 #include <string>
+#include <cstring>
 #include <vector>
 #include <list>
 #include <set>
 #include <map>
+#include <queue>
 using namespace std;
 
-class MyComp{
+
+class MazeFather{
 public:
-    bool operator()(pair<int,int> p1,pair<int ,int>p2){
-        return p1.second<p2.second;
-    }
+    virtual void inputData()=0;
+    virtual void findPath()=0;
+    virtual void outputPath()=0;
+    virtual ~MazeFather() = default;
 };
 
-struct Polynomial{
-    typedef  map<int,int> myType;
-    //第一项是指数，第二项是系数
-    myType body;
+class Maze: public MazeFather{
+private:
+    int n,m,startX,startY,endX,endY;
+    int dfsStopFlag;
+    vector<vector<int> > mazeMatrix;
+    vector<vector<int> > dis;
 
-    Polynomial operator+(const Polynomial& other)const{
-        Polynomial temp=*this;
-
-        for(auto &it:other.body){
-            temp.body[it.first]+=it.second;
-            if(temp.body[it.first]==0)
-                temp.body.erase(it.first);
+    struct Node{
+        Node(int xx, int yy)
+        : x(xx),y(yy),cost(0),f(0),direction(-1){}
+        Node(int xx, int yy, int c,int f)
+        : x(xx),y(yy),cost(c),f(f),direction(-1){}
+        Node(int xx, int yy, int c,int f, int d)
+        : x(xx),y(yy),cost(c),f(f),direction(d){}
+        int x,y;
+        int cost=0;
+        int f=0;
+        int direction=-1;
+        bool operator < (const Node other)const{
+            return f < other.f;
         }
-        return temp;
+        bool operator == (const Node other)const{
+            return x==other.x&&y==other.y;
+        }
+    };
+    vector<vector<Node> > pre;
+    Node direction[4]={{0,1},  {1,0},
+                       {0,-1},  {-1,0}};
+
+    //判断越界
+    bool beyondBoundary(int x, int y) const{
+        if(x<0||y<0||x>=n||y>=m)
+            return true;
+        return false;
+    }
+    //计算曼哈顿距离
+    static int manhattanDis(int ax,int ay, int bx,int by){
+        return abs(ax-bx)+ abs(ay-by);
     }
 
-    Polynomial operator-(const Polynomial& other)const{
-        Polynomial temp= *this;
+//A* algorithm ,题目要求的是用深度优先搜索
+    void AStar(){
+        priority_queue<Node> q;
+        int manhattan= manhattanDis(startX,startY,endX,endY);
+        q.push({startX,startY,0,manhattan});
+        dis[startX][startY]=0;
+        while(!q.empty()){
+            Node curNode=q.top();
+            q.pop();
+            for(int i=0;i<4;i++){
+                int newX=curNode.x+direction[i].x;
+                int newY=curNode.y+direction[i].y;
+                int newCost=curNode.cost+1;
+                //判断越界
+                if(beyondBoundary(newX,newY))
+                    continue;
+                if(mazeMatrix[newX][newY])
+                    continue;
+                if(dis[newX][newY]>newCost){
+                    dis[newX][newY]=newCost;
+                    curNode.direction=i;
+                    pre[newX][newY]=curNode;
 
-        for(auto &it:other.body){
-            temp.body[it.first]-=it.second;
-            if(temp.body[it.first]==0)
-                temp.body.erase(it.first);
+                    int f=newCost + manhattanDis(newX,newY,
+                                                 endX,endY);
+                    q.push({newX,newY,newCost,f});
+                }
+            }
         }
-        return temp;
     }
 
-    friend ostream& operator<<(ostream& os,const Polynomial &sub){
-        bool first= true;
-        if(sub.body.empty())
-            return os<<'0';
-        for(const auto &it:sub.body){
-            if(!first){
-                if(it.second>0)
-                    os<<'+';
-                else
-                    os<<'-';
-            }
-            first= false;
-            if(it.first==0)
-                os<<it.second;
-            else {
-                if(it.second!=1&&it.second!=-1)
-                    os<<abs(it.second);
-                os << "x" ;
-                if(it.first!=1)
-                    os<<'^'<<it.first;
-            }
+
+
+    void dfs(int x,int y, int cost){
+        if(x==endX&&y==endY){
+            dfsStopFlag=1;
+
+            return;
         }
-        return os;
+        if(dfsStopFlag)
+            return;
+        if(!dis[x][y])
+            return;
+        for(int i=0;i<4;i++){
+            int newX=x+direction[i].x;
+            int newY=y+direction[i].y;
+            int newCost=cost+1;
+            //判断越界
+            if(beyondBoundary(newX,newY))
+                continue;
+            if(!dis[newX][newY])
+                continue;
+            if(mazeMatrix[newX][newY])
+                continue;
+            pre[newX][newY]=Node({x,y,cost,0,i});
+            dis[x][y]=0;
+            dfs(newX,newY,newCost);
+            //dis[newX][newY]=1;
+            if(dfsStopFlag)
+                break;
+        }
+    }
+
+    void dfsStart(){
+        //dis[startX][startY]=0;
+        dfs(startX,startY,0);
+    }
+
+    //递归调用输出
+    void recursionOutput(int x,int y,int d){
+        Node &preNode=pre[x][y];
+        if(preNode==Node({-1,-1})){
+            cout<<'('<<x+1<<','<<y+1<<','<<d+1 <<')';
+            return;
+        }
+        recursionOutput(preNode.x,preNode.y,preNode.direction);
+
+        cout<<",("<<x+1<<','<<y+1<<','<<d+1 <<')';
+    }
+public:
+    Maze(){    }
+
+    //输入数据和初始化
+    void inputData() override{
+        cin>>n>>m>>startX>>startY>>endX>>endY;
+        vector<int> tmp;
+        tmp.resize(m);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++)
+                cin >> tmp[j];
+            mazeMatrix.push_back(tmp);
+        }
+        --startX,--startY,--endX,--endY;
+        //初始化dis
+        tmp.assign(m,0x3fffffff);
+        for(int i=0;i<n;i++)
+            dis.push_back(tmp);
+        //初始化pre，pre表示路径上某个点的上一个点数据
+        vector<Node> tmpNode;
+        tmpNode.assign(m,{-1,-1});
+        for(int i=0;i<n;i++)
+            pre.push_back(tmpNode);
+        dfsStopFlag=0;
+
+    }
+
+    //path finding algorithm
+    void findPath() override{
+        dfsStart();
+    }
+
+    void outputPath() override{
+        if(pre[endX][endY].direction!=-1)
+            recursionOutput(endX,endY,0);
+        else
+            cout<<"no";
     }
 };
 
 int main()
 {
-    int n,m,operate;
-    int temp1,temp2;
-    Polynomial p1,p2;
-    cin>>n>>m>>operate;
-    while(n--){
-        cin>>temp1>>temp2;
-        p1.body.insert({temp2,temp1});
-    }
-    while(m--){
-        cin>>temp1>>temp2;
-        p2.body.insert({temp2,temp1});
-    }
-    if(operate)
-        cout<<p1-p2;
-    else
-        cout<<p1+p2;
+    MazeFather* solveP = new Maze();
+    solveP->inputData();
+    solveP->findPath();
+    solveP->outputPath();
+    delete(solveP);
     return 0;
 }
