188ff9928a03aff5119dc79b237ba6efed4ba61c
1123 <1079733103@qq.com>
2024-11-23 12:46:54 +0800

    密码破译

--- main.cpp
+++ main.cpp
@@ -12,5 +12,13 @@ using namespace std;
 
 int main()
 {
+    string s;
+    cin>>s;
+    for(auto &tmpC:s){
+        tmpC+=4;
+        if((tmpC>'z') || (tmpC<'a'&&tmpC>'Z'))
+            tmpC-=26;
+    }
+    cout<<s;
     return 0;
 }
