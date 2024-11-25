68629840f262302207e39f7394003b0a13709b73
1123 <1079733103@qq.com>
2024-11-25 22:22:40 +0800

    Fibonacci数列

--- main.cpp
+++ main.cpp
@@ -10,10 +10,19 @@
 #include <algorithm>
 using namespace std;
 
+int fib(int n){
+    if(n==0)
+        return 7;
+    if(n==1)
+        return 11;
+    return fib(n - 1) + fib(n - 2);
+}
 
 int main()
 {
 
-
+    int n;
+    cin>>n;
+    cout << fib(n);
     return 0;
 }
